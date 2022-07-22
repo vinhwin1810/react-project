59327f0b45e3e7b89605faf8343d1cc2f2438a37
Your Name <you@example.com>
2022-07-22 00:32:24 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from "react";
+import '.Navbar.css';
+
+function Navbar(){
+    const[click, setClick] = useState(false);
+    const[button, setButton] = useState(true)
+}
